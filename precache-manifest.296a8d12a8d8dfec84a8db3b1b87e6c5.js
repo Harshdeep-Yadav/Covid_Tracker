01314328d82ddda687040cc1aa87005552816b39
Harshdeep Yadav <harshdeepy5708@gmail.com>
2022-09-02 19:31:10 +0530

    Updates

---
+++ precache-manifest.296a8d12a8d8dfec84a8db3b1b87e6c5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ece3ffe39ccde694c4d0081d02bc7c61",
+    "url": "/Covid_Tracker/index.html"
+  },
+  {
+    "revision": "3082ce160e510a5ff1b6",
+    "url": "/Covid_Tracker/static/css/main.c53b8684.chunk.css"
+  },
+  {
+    "revision": "eedae24986149cce7c52",
+    "url": "/Covid_Tracker/static/js/2.bcabd233.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/Covid_Tracker/static/js/2.bcabd233.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3082ce160e510a5ff1b6",
+    "url": "/Covid_Tracker/static/js/main.46855c67.chunk.js"
+  },
+  {
+    "revision": "2a121428d6fdc1e4072f",
+    "url": "/Covid_Tracker/static/js/runtime-main.a01d64af.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/Covid_Tracker/static/media/corona.d7265326.png"
+  }
+]);
